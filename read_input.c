2955aee944a0721354b3bf502bd90ab650c31fc8
Raul Alhena <38526688+raulalhena@users.noreply.github.com>
2021-12-21 16:15:20 +0100

    added content

--- read_input.c
+++ read_input.c
@@ -1,1 +1,11 @@
+#include "functions.h"
 
+char    *read_input(char **p)
+{
+    int a;
+    a = read(0, *p, SIZE);
+    *p = malloc(sizeof(char) * a);
+    if (!*p)
+        return ("Memory error\n");
+    return (*p);
+}
