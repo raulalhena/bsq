daec24888e65b80fc3499e7c1cca7b4673304ba2
Raul Alhena <38526688+raulalhena@users.noreply.github.com>
2021-12-21 15:55:43 +0100

    first commit

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+#include "functions.h"
 
+int main(int argc, char *argv[])
+{
+  t_map *map;
+  int i;
+  char *p;
+  
+  if (argc > 1)
+  {
+    i = 1;
+    while (i < argc - 1)
+    {
+      validate_map(read_file(argv[i], p), map);
+      resolve_map(map);
+      print_map(map);
+    }
+   else if (argc == 1)
+   {
+     validate_map(read_input(argv[1], p), map);
+     resolve_map(map);
+     print_map(map);
+   }
+}
