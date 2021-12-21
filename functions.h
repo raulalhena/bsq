5df450776950cae96a0de687aaf378c5d796e163
Raul Alhena <38526688+raulalhena@users.noreply.github.com>
2021-12-21 11:23:24 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -11,9 +11,13 @@ typedef struct s_map{
   int map_size;
 } t_map;
 
-char  *read_input(void);
-char  *read_file(char *argv);
-int validate_map(char *buff);
-void  resolve_map(char *buff, t_map *map);
-void  print_map(t_map map);
-void  free_map(t_map *map);
+char  *read_input(void); // Miguel
+char  *read_file(char *argv); // Miguel
+int   validate_map(char *buff); // Albert
+void  resolve_map(char *buff, t_map *map); // Gabriel
+void  print_map(t_map map); // Julia
+void  free_map(t_map *map); // Julia
+int   ft_atoi(char *str); // to customize
+int   ft_strlen(char *str);
+void  ft_putchar(char c);
+void  ft_putstr(char *str);
