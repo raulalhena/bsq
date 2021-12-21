abe88220660ac795d4ae5975ff7ae02ac49f8ed7
Gabriel Pujol rojo <gpujol-r@cbr2s1.42barcelona.com>
2021-12-21 16:33:49 +0100

    norminette

--- read_file.c
+++ read_file.c
@@ -6,34 +6,37 @@
 /*   By: mfort-pe <mfort-pe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 12:24:35 by mfort-pe          #+#    #+#             */
-/*   Updated: 2021/12/21 15:35:24 by mfort-pe         ###   ########.fr       */
+/*   Updated: 2021/12/21 16:31:00 by gpujol-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "functions.h"
-char    *read_file(char *filename, char **p)
+
+char	*read_file(char *filename, char **p)
 {
-    int     fd;
-    char    buff[SIZE];
-    int     count;
-    int     a;
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return ("File error\n");
-    count = 0;
-    a = 1;
-    while (a)
-    {
-        a = read(fd, buff, SIZE);
-        count += a;
-    }
-    *p = malloc(sizeof(char) * count);
-    if (!*p || close(fd))
-        return ("Error\n");
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return ("File error\n");
-    read(fd, *p, count);
-    if (close(fd))
-        return ("File error\n");
-    return (*p);
+	int		fd;
+	char	buff[SIZE];
+	int		count;
+	int		a;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return ("File error\n");
+	count = 0;
+	a = 1;
+	while (a)
+	{
+		a = read(fd, buff, SIZE);
+		count += a;
+	}
+	*p = malloc(sizeof(char) * count);
+	if (!*p || close(fd))
+		return ("Error\n");
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return ("File error\n");
+	read(fd, *p, count);
+	if (close(fd))
+		return ("File error\n");
+	return (*p);
 }
--- resolve_map.c
+++ resolve_map.c
@@ -6,13 +6,13 @@
 /*   By: gpujol-r <gpujol-r@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 11:34:47 by gpujol-r          #+#    #+#             */
-/*   Updated: 2021/12/21 16:08:28 by gpujol-r         ###   ########.fr       */
+/*   Updated: 2021/12/21 16:28:22 by gpujol-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "functions.h"
 
-void  resolve_map(char *buff, t_map *map)
+void	resolve_map(char *buff, t_map *map)
 {
 	if (create_map(map))
 		return (ft_putstr("Map error\n"));
