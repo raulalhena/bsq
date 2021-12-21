cd2564367fc0ed2c71f0f24643ccd8975cffcb82
Gabriel Pujol rojo <gpujol-r@cbr2s1.42barcelona.com>
2021-12-21 20:17:53 +0100

    fixed validate map

--- validate_map.c
+++ validate_map.c
@@ -6,7 +6,7 @@
 /*   By: amantara <amantara@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 11:38:26 by amantara          #+#    #+#             */
-/*   Updated: 2021/12/21 19:39:28 by rlopez-m         ###   ########.fr       */
+/*   Updated: 2021/12/21 20:17:34 by gpujol-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,8 +102,12 @@ int	validate_map(char *buff, t_map *map)
 {
 	int	num_rows;
 	int	num_cols;
+	char *first_line;
 
-	num_rows = validate_print_char(buff, map);
+	first_line = malloc(ft_strlen2(buff) * sizeof(char));
+	if (!first_line)
+		return (print_error(2));
+	num_rows = validate_print_char(first_line, map);
 	if (validate_row_number(buff, num_rows) == -1)
 		return (print_error(3));
 	else if (check_char(buff, map->obstacle, map->empty, num_rows) == -1)
