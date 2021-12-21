8a1e00dc5b964426d2b3e1cfcd324153ba794521
Gabriel Pujol rojo <gpujol-r@cbr2s1.42barcelona.com>
2021-12-21 12:11:20 +0100

    stuff

--- find_square.c
+++ find_square.c
@@ -6,11 +6,13 @@
 /*   By: gpujol-r <gpujol-r@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 12:01:16 by gpujol-r          #+#    #+#             */
-/*   Updated: 2021/12/21 12:03:14 by gpujol-r         ###   ########.fr       */
+/*   Updated: 2021/12/21 12:10:58 by gpujol-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "functions.h"
+
+//algoritmo
 
 void	find_square(t_map map)
 {
