c4812bf8a3c87297cedeeebe32ec1521983c0f13
Raul Lopez martin <rlopez-m@car9s2.42barcelona.com>
2021-12-21 19:22:20 +0100

    validate & utils1.c

--- utils1.c
+++ utils1.c
@@ -6,10 +6,40 @@
 /*   By: rlopez-m <rlopez-m@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 16:34:39 by rlopez-m          #+#    #+#             */
-/*   Updated: 2021/12/21 16:34:41 by rlopez-m         ###   ########.fr       */
+/*   Updated: 2021/12/21 19:18:08 by ffusalba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "functions.h"
 
-// atoi
+unsigned int	ft_atoi(char *str)
+{
+	unsigned int	number;
+
+	number = 0;
+	while (*str != '\0' && *str >= '0' && *str <= '9')
+	{
+		number = number * 10;
+		number = number + (*str - '0');
+		str++;
+	}
+	return (number);
+}
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	aux;
+
+	aux = 0;
+	while (src[aux] && aux < n)
+	{
+		dest[aux] = src[aux];
+		aux++;
+	}
+	while (aux < n)
+	{
+		dest[aux] = '\0';
+		aux++;
+	}
+	return (dest);
+}
---
+++ validatelines.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validatelines.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffusalba <ffusalba@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 12:01:41 by ffusalba          #+#    #+#             */
+/*   Updated: 2021/12/21 19:18:02 by ffusalba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "functions.h"
+
+int	validatecommand(char *str, t_map *map)
+{
+	int			size;
+	const char	*last_three = &str[size - 3];
+
+	size = ft_strlen(str);
+	if (last_three[0] == last_three[1] || last_three[1] == last_three[2]
+		|| last_three[0] == last_three[2])
+	{
+		ft_putstr("Error");
+		return (0);
+	}
+	else
+	{
+		map = malloc(sizeof(t_map));
+		map->empty = last_three[0];
+		map->obstacle = last_three[1];
+		map->full = last_three[2];
+		return (1);
+	}
+}
+
+unsigned int	validatenumber(char *str, t_map *map)
+{
+	unsigned int	size;
+	int				size3;
+	int				i;
+	char			*numrows;
+
+	i = 0;
+	size = ft_strlen(str);
+	numrows = malloc(sizeof(char) * (size - 3));
+	numrows = ft_strncpy(numrows, str, size - 3);
+	size3 = size - 3;
+	while (numrows[i])
+	{
+		if (ft_strlen(numrows) > 10 || numrows[i] < '0' || numrows[i] > '9')
+		{
+			ft_putstr("Error");
+			return (0);
+		}
+		else if (validatecommand(str, map))
+			return (ft_atoi(numrows));
+		i++;
+	}
+	return (0);
+}
+
+unsigned int	validateprintchar(char *str, t_map *map)
+{
+	int				commandsize;
+	unsigned int	finalnumber;
+	int				i;
+
+	finalnumber = 0;
+	i = 0;
+	commandsize = ft_strlen(str);
+	while (str[i] != '\0')
+	{
+		if (commandsize >= 4 && commandsize <= 13)
+		{
+			if (str[i] < ' ' || str[i] > '~')
+			{
+				ft_putstr("Error");
+				return (0);
+			}
+		}
+		else if (commandsize < 4 || commandsize > 13)
+		{
+			ft_putstr("Error");
+			return (0);
+		}
+		i++;
+	}
+	return (validatenumber(str, map));
+}
