29eef919d8446db400fbc4b30d897a89d2c7e971
Raul Alhena <38526688+raulalhena@users.noreply.github.com>
2021-12-21 13:27:26 +0100

    added ft_strcpy

--- utils2.c
+++ utils2.c
@@ -26,3 +26,16 @@ void  ft_putstr(char *str)
     i++;
   }
 }
+
+char    *ft_strcpy(char *dest, char *src)
+{
+    int i;
+    i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
