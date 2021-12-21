1dfff05377a58d825b1a2cae68b6def89f657074
Albert Mantaras paricio <amantara@car13s3.42barcelona.com>
2021-12-21 16:56:43 +0100

    Atoi

--- utils1.c
+++ utils1.c
@@ -1,3 +1,15 @@
 #include "functions.h"
 
-// atoi
+int ft_atoi(char *str)
+{
+    int number;
+
+    number = 0;
+    while (*str >= '0' && *str <= '9')
+    {
+        number = number * 10;
+        number = number + (*str - '0');
+        str++;
+    }
+    return (number);
+}
