31eb4432848ace3b12f1e779dbd53aafe71a7f71
Gabriel Pujol rojo <gpujol-r@cbr2s1.42barcelona.com>
2021-12-21 11:47:28 +0100

    changed struct

--- functions.h
+++ functions.h
@@ -1,23 +1,45 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fnctl.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpujol-r <gpujol-r@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 11:41:21 by gpujol-r          #+#    #+#             */
+/*   Updated: 2021/12/21 11:43:14 by gpujol-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNCTIONS_H
+# define FUNCTIONS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fnctl.h>
+# include <stdlib.h>
 
 typedef struct s_map{
-  char **map;
-  int start_x;
-  int start_y;
-  int max;
-  int map_size;
-} t_map;
+	char	**map;
+	int		start_x;
+	int		start_y;
+	int		max;
+	int		map_size;
+	int		rows;
+	int		cols;
+	char	empty;
+	char	obstacle;
+	char	full;
+}	t_map;
 
-char  *read_input(void); // Miguel
-char  *read_file(char *argv); // Miguel
-int   validate_map(char *buff); // Albert
-void  resolve_map(char *buff, t_map *map); // Gabriel
-void  print_map(t_map map); // Julia
-void  free_map(t_map *map); // Julia
-int   ft_atoi(char *str); // to customize
-int   ft_strlen(char *str);
-void  ft_putchar(char c);
-void  ft_putstr(char *str);
+char	*read_input(void); // Miguel
+char	*read_file(char *argv); // Miguel
+int		validate_map(char *buff); // Albert
+void	resolve_map(char *buff, t_map *map); // Gabriel
+void	print_map(t_map map); // Julia
+void	free_map(t_map *map); // Julia
+int		ft_atoi(char *str); // to customize
+int		ft_strlen(char *str);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+#endif
